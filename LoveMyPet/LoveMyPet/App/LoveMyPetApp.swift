e2a00e5f146135093330162a057e952df10a4656
DylanSkywalker <114362937+DylanSkywalker@users.noreply.github.com>
2023-07-05 21:10:12 -0300

    Set up

---
+++ LoveMyPet/LoveMyPet/App/LoveMyPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveMyPetApp.swift
+//  LoveMyPet
+//
+//  Created by userext on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LoveMyPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
