409d622087b45454d7b350a7bdfa59fcb04878e7
DouglasCFarias <douglas.farias62@aluno.ifce.edu.br>
2023-08-01 17:38:37 -0300

    Modificacoes no ImagePicker e no icone do RacePicker

---
+++ LoveMyPet/LoveMyPet/Features/FileManager/ImageFileManager.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+class ImageFileManager {
+
+    func loadImage(named imageName: String) -> UIImage? {
+        if let image = UIImage(named: imageName) {
+            return image
+        } else {
+            return nil
+        }
+    }
+    func saveImage(image: UIImage, withName imageName: String) -> Bool {
+        guard let imageData = image.jpegData(compressionQuality: 1.0) else {
+            return false
+        }
+        let fileURL = getDocumentsDirectory().appendingPathComponent("\(imageName).jpg")
+        do {
+            try imageData.write(to: fileURL)
+            return true
+        } catch {
+            return false
+        }
+    }
+
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+
+}
