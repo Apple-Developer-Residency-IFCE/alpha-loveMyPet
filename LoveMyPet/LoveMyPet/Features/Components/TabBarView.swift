75affae52cbdc28a05040a3a0d0b55ad5bec7329
snp235 <108408380+snp235@users.noreply.github.com>
2023-08-04 18:24:53 -0300

    TabBar

---
+++ LoveMyPet/LoveMyPet/Features/Components/TabBarView.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarView.swift
+//  LoveMyPet
+//
+//  Created by userext on 01/08/23.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
---
+++ LoveMyPet/LoveMyPet/Features/Perfís/ProfilesView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfilesView.swift
+//  LoveMyPet
+//
+//  Created by userext on 01/08/23.
+//
+
+import SwiftUI
+
+struct ProfilesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfilesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilesView()
+    }
+}
