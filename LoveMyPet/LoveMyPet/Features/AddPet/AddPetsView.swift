ca76aebc151b22153b3f4ea771deb27ed9409497
DouglasCFarias <douglas.farias62@aluno.ifce.edu.br>
2023-08-04 21:47:09 -0300

    AddPetsView

---
+++ LoveMyPet/LoveMyPet/Features/AddPet/AddPetsView.swift
@@ -0,0 +1,20 @@
+//
+//  AddPetsView.swift
+//  LoveMyPet
+//
+//  Created by userext on 03/08/23.
+//
+
+import SwiftUI
+
+struct AddPetsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddPetsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPetsView()
+    }
+}
